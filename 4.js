64c3674b9a1e617c36d82fa6aeb11f8bf3c900df
Noman Mia <nomanislamnishad@gmail.com>
2025-01-26 21:03:10 +0600

    added arrow function

--- 4.js
+++ 4.js
@@ -1,11 +1,20 @@
-//function in js
-function addTwoNumber(addOne, addTwo){
-    // let addOne = 20;
-    // let addTwo = 10;
-    let addAll = addOne + addTwo;
-   return addAll;
+// //function in js
+// function addTwoNumber(addOne, addTwo){
+//     // let addOne = 20;
+//     // let addTwo = 10;
+//     let addAll = addOne + addTwo;
+//    return addAll;
 
-}
+// }
 
-const add = addTwoNumber(2,4);
-console.log(add);
+// const add = addTwoNumber(2,4);
+// console.log(add);
+
+let add = ((a,b)=>{
+let first = a;
+let second = b;
+let addTwo = first + second;
+return addTwo;
+})
+ let addd = add(30,20);
+ console.log(addd);
