0eb4637a25cf1ec10f2b4c6cb648646c51c6475f
Noman Mia <nomanislamnishad@gmail.com>
2025-01-27 07:42:55 +0600

    add const myArray = [54, 85, 87, 47]; //array

---
+++ destructuring/8.js
@@ -0,0 +1,21 @@
+//destructuring
+
+const myArray = [54, 85, 87, 47]; //array
+const [x, y, z, w] = myArray;
+console.log(x, y, z, w);
+
+const man = {
+  name: "noMan", //object
+  rolls: 677319,
+  age: 20,
+  home: {
+    mainHome: "RowMari",
+    home: "kurGram",
+    village: {
+      nam: "BolMari",
+    },
+  },
+};
+
+//   const {rolls} = man;
+//   console.log(rolls);
